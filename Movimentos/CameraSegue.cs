16c645b8d8f8eb357232a910e772395f12013cff
Junior de Paula <jhum.dp@gmail.com>
2019-09-15 23:53:24 -0300

    Adicionado mais alguns scripts e Shader

---
+++ Movimentos/CameraSegue.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CameraSegue : MonoBehaviour {
+
+	[SerializeField] Transform alvo; 
+	[SerializeField] float velocidadeSuave = 30f; 
+	[SerializeField] Vector3 deslocamento; 
+	Vector3 posicaoDesejada; 
+	Vector3 posicaoSuavizada; 
+	Vector3 velocidade = Vector3.zero;
+
+	void LateUpdate() 
+	{
+		posicaoDesejada = alvo.position + deslocamento;
+		posicaoSuavizada = Vector3.SmoothDamp(transform.position, posicaoDesejada, ref velocidade, velocidadeSuave * Time.deltaTime);
+		transform.position = posicaoSuavizada;
+	}
+}
---
+++ Pool/ObjectPooling.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooling : MonoBehaviour {
+
+	//Basico para spawn por tempo no local desse script.. Pode ser ajusto de varias formas.
+
+	[SerializeField] int quantidade;
+	[SerializeField] float tempoEntreSpawn;
+	public GameObject[] objetos;
+	List<GameObject> listObjetos;
+	float tempo;
+	int randon; // Criar objetos randomicos
+
+	void Awake () {
+
+		listObjetos = new List<GameObject>(quantidade);
+		for (int i = 0; i < quantidade; i++)
+		{
+			randon = Random.Range(0, objetos.Length);
+			GameObject spiritInstance = Instantiate(objetos[randon], transform.position, Quaternion.identity);
+			spiritInstance.transform.SetParent(transform);
+			spiritInstance.SetActive(false);
+			listObjetos.Add(spiritInstance);
+		}
+	}
+	
+	void Update () {
+		
+		tempo += Time.deltaTime;
+		if (tempo >= tempoEntreSpawn)
+		{
+			SpawInimigos();
+			tempo = 0;
+		}
+	}
+
+	public GameObject SpawInimigos() // Metodo para spawn dos objetos
+	{
+		foreach (GameObject inimigo in listObjetos)
+		{
+			if (!inimigo.activeInHierarchy)
+			{
+				inimigo.transform.position = transform.position;
+				inimigo.SetActive(true);
+				return inimigo;
+			}
+		}
+		return null;
+	}
+}
